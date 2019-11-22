50a3b02647bbfeb103a5df48e7aa53b9d4cc3377
shanzhi <choukanan@gmail.com>
2019-11-22 17:26:11 +0800

    增加测试接口

---
+++ spring-boot-service/src/main/java/com/white/service/test/TestService.java
@@ -0,0 +1,9 @@
+package com.white.service.test;
+
+/**
+ * @author 山栀
+ * @date 2019/11/22 17:21
+ */
+public interface TestService {
+
+}
---
+++ spring-boot-service/src/main/java/com/white/service/test/impl/TestServiceImpl.java
@@ -0,0 +1,14 @@
+package com.white.service.test.impl;
+
+import com.white.service.test.TestService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 山栀
+ * @date 2019/11/22 17:22
+ */
+@Service
+public class TestServiceImpl  implements TestService {
+
+
+}
