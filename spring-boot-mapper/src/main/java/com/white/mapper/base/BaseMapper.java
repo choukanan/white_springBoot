a4720a4b94b94ec795de42c8965caebb8bcc9bcd
shanzhi <choukanan@gmail.com>
2019-11-22 17:38:39 +0800

    mapper结构调整

---
+++ spring-boot-mapper/src/main/java/com/white/mapper/base/BaseMapper.java
@@ -0,0 +1,8 @@
+package com.white.mapper.base;
+
+/**
+ * @author 山栀
+ * @date 2019/11/22 17:37
+ */
+public interface BaseMapper {
+}
---
+++ spring-boot-mapper/src/main/java/com/white/mapper/test/TestMapper.java
@@ -0,0 +1,8 @@
+package com.white.mapper.test;
+
+/**
+ * @author 山栀
+ * @date 2019/11/22 17:34
+ */
+public interface TestMapper {
+}
